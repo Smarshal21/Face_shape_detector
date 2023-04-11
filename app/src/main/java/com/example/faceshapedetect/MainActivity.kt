7af1deaab7ef0e488f61d23f4c9127bd23a79aa6
Smarshall <99678760+Smarshal21@users.noreply.github.com>
2023-04-11 15:43:05 +0530

    fixedcode

--- app/src/main/java/com/example/faceshapedetect/MainActivity.kt
+++ app/src/main/java/com/example/faceshapedetect/MainActivity.kt
@@ -15,6 +15,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.support.common.FileUtil
@@ -24,16 +26,43 @@ import android.content.Intent as Intent1
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var select_image_button : Button
-    lateinit var make_prediction : Button
-    lateinit var img_view : ImageView
-    lateinit var text_view : TextView
+    private lateinit var select_image_button : Button
+    private lateinit var make_prediction : Button
+    private lateinit var img_view : ImageView
+    private lateinit var predictedLabel : String
+    private lateinit var text_view : TextView
+    private lateinit var camerabtn : Button
     var bitmap:Bitmap? = null
-    lateinit var camerabtn : Button
     var uri:String = ""
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        select_image_button = findViewById(R.id.button)
+        make_prediction = findViewById(R.id.button2)
+        img_view = findViewById(R.id.imageView2)
+        text_view = findViewById(R.id.textView)
+        camerabtn = findViewById(R.id.camerabtn)
+        checkandGetpermissions()
+
+
+        select_image_button.setOnClickListener {
+            Log.d("mssg", "button pressed")
+            var intent = Intent1(Intent1.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 250)
+        }
+
+        camerabtn.setOnClickListener {
+            var camera = Intent1(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(camera, 200)
+        }
+
+    }
+
 
     @RequiresApi(Build.VERSION_CODES.M)
-    public fun checkandGetpermissions(){
+    private fun checkandGetpermissions(){
         if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
             requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
         }
@@ -60,37 +89,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-        select_image_button = findViewById(R.id.button)
-        make_prediction = findViewById(R.id.button2)
-        img_view = findViewById(R.id.imageView2)
-        text_view = findViewById(R.id.textView)
-        camerabtn = findViewById<Button>(R.id.camerabtn)
-        checkandGetpermissions()
-
-
-        select_image_button.setOnClickListener(View.OnClickListener {
-            Log.d("mssg", "button pressed")
-            var intent : Intent1 = Intent1(Intent1.ACTION_GET_CONTENT)
-            intent.type = "image/*"
-            startActivityForResult(intent, 250)
-        })
-        camerabtn.setOnClickListener(View.OnClickListener {
-            var camera : Intent1 = Intent1(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(camera, 200)
-        })
-
-    }
-
-
     @SuppressLint("SuspiciousIndentation")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent1?) {
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent1?) {
         super.onActivityResult(requestCode, resultCode, data)
+
 
         if(requestCode == 250){
             img_view.setImageURI(data?.data)
@@ -103,32 +105,33 @@ class MainActivity : AppCompatActivity() {
             bitmap = data?.extras?.get("data") as Bitmap
             img_view.setImageBitmap(bitmap)
         }
-        fun ARGBBitmap(img: Bitmap): Bitmap {
-            return img.copy(Bitmap.Config.ARGB_8888, true)
+        make_prediction.setOnClickListener {
+            makeprediction()
         }
 
-
-
-        make_prediction.setOnClickListener(View.OnClickListener {
-            val image = TensorImage.fromBitmap(ARGBBitmap(bitmap!!)).bitmap
-            val inputWidth = 250
-            val inputHeight = 190
-            val inputBuffer = TensorBuffer.createFixedSize(intArrayOf(1, inputHeight, inputWidth, 1), DataType.FLOAT32)
-            val resizedBitmap = Bitmap.createScaledBitmap(image, inputWidth, inputHeight, true)
-            val pixelValues = convertBitmapToFloatArray(resizedBitmap)
-            inputBuffer.loadArray(pixelValues)
-            val inputTensorBuffer = inputBuffer.buffer
-            val tfliteModel = FileUtil.loadMappedFile(applicationContext, "face_shape_recognizer.tflite")
-            val tflite: Interpreter = Interpreter(tfliteModel)
-            val outputBuffer = TensorBuffer.createFixedSize(intArrayOf(1, 5), DataType.FLOAT32)
-            tflite.run(inputTensorBuffer, outputBuffer.buffer)
-            val labels = listOf("Heart", "Oblong", "Oval", "Round", "Square")
-            val outputArray = outputBuffer.floatArray
-            val maxIndex = outputArray.indices.maxByOrNull { outputArray[it] } ?: -1
-            val predictedLabel = labels[maxIndex]
-            text_view.text = predictedLabel
-            tflite.close()
-        }   )
+    }
+    fun ARGBBitmap(img: Bitmap): Bitmap {
+        return img.copy(Bitmap.Config.ARGB_8888, true)
+    }
+    fun makeprediction(){
+        val image = TensorImage.fromBitmap(ARGBBitmap(bitmap!!)).bitmap
+        val inputWidth = 250
+        val inputHeight = 190
+        val inputBuffer = TensorBuffer.createFixedSize(intArrayOf(1, inputHeight, inputWidth, 1), DataType.FLOAT32)
+        val resizedBitmap = Bitmap.createScaledBitmap(image, inputWidth, inputHeight, true)
+        val pixelValues = convertBitmapToFloatArray(resizedBitmap)
+        inputBuffer.loadArray(pixelValues)
+        val inputTensorBuffer = inputBuffer.buffer
+        val tfliteModel = FileUtil.loadMappedFile(applicationContext, "face_shape_recognizer.tflite")
+        val tflite: Interpreter = Interpreter(tfliteModel)
+        val outputBuffer = TensorBuffer.createFixedSize(intArrayOf(1, 5), DataType.FLOAT32)
+        tflite.run(inputTensorBuffer, outputBuffer.buffer)
+        val labels = listOf("Heart", "Oblong", "Oval", "Round", "Square")
+        val outputArray = outputBuffer.floatArray
+        val maxIndex = outputArray.indices.maxByOrNull { outputArray[it] } ?: -1
+        predictedLabel = labels[maxIndex]
+        text_view.text = predictedLabel
+        tflite.close()
 
     }
     fun convertBitmapToFloatArray(bitmap: Bitmap): FloatArray {
